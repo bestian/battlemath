e88e7948d7890499f94c2f65d0ff958a7a992c14
Bestian Tang <bestian@gmail.com>
2021-06-10 19:59:11 +0800

    init

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,70 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="container">
+      <q-linear-progress :value="time / 100" color="warning" class="q-mt-md" />
+      <h4>您的分數:{{you}}</h4>
+      <h4>敵人的分數:{{enemy}}</h4>
+      <h3>{{n}} + {{m}} = ?</h3>
+      <q-input type="number" v-model="ans" @input = "check()"/>
+      <q-btn color="primary" size ="xl" v-if = "win" @click="again()">你勝利了，按此重來</q-btn>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  props: ['d'],
+  data() {
+    return {
+      time: 0,
+      you: 0,
+      enemy: 0,
+      n:3,
+      m:4,
+      ans: null,
+      win: false
+    }
+  },
+  methods: {
+    check() {
+      if (this.ans == this.n + this.m) {
+        this.you++;
+        this.time = 0;
+        this.reset()
+      }
+      if (this.you > this.enemy + 10) {
+        this.win = true
+      }
+    },
+    again() {
+      this.win = false
+      this.reset()
+    },
+    reset() {
+      this.n = Math.floor(Math.random()*this.d) + 1;
+      this.m = Math.floor(Math.random()*this.d) + 1;
+      this.ans = null;
+    },
+    go() {
+      this.time++;
+      if (this.time >= 100) {
+        this.enemy++
+        this.reset()
+        this.time = 0
+      }
+    }
+  },
+  mounted() {
+    this.reset()
+    setInterval(this.go, 100)
+  }
 }
 </script>
+
+<style type="text/css" scoped="">
+  .container {
+    max-width: 620px;
+    margin: 0 auto;
+  }
+</style>
